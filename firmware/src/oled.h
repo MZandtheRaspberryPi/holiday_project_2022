d37dd95874bb38ae7da342996dc9f4fa67139d02
MZandtheRaspberryPi <ziegltrum@gmail.com>
2022-12-13 22:23:47 +0100

    adding facial animations

--- firmware/src/oled.h
+++ firmware/src/oled.h
@@ -3,7 +3,7 @@
 
 #include "stdint.h"
 
-#define TASK_OLED_FREQUENCY 1                             // Hz
+#define TASK_OLED_FREQUENCY 100                             // Hz
 #define TASK_OLED_INTERVAL  (1000 / TASK_OLED_FREQUENCY)   // ms
 
 typedef enum {
@@ -21,5 +21,11 @@ bool Task_OLED_Setup(void);
 void Task_OLED_Periodic(void);
 void Timeline_Draw_Data_Point(uint16_t dataPoint, uint16_t scale, FRAME_Part_e framePart = FRAME_PART_FULL);
 void Plot_ADC_Dump(uint16_t* data, uint16_t size);
+
+void displayFace(uint8_t left_eye_y_offset, uint8_t right_eye_y_offset, uint8_t mouth_y_offset);
+void blinkEyes(const uint16_t counter, const uint16_t end_count);
+void actuateMouth(const uint16_t counter, const uint16_t end_count);
+void squintEyes(const uint16_t counter, const uint16_t end_count);
+void animateFace();
 
 #endif // OLED_H
